8af788e561d03e9270c5509caa39864396807699
somenmahato124-netizen <somenmahato124@gmail.com>
2026-01-15 19:52:11 +0530

    Add files via upload

--- bookingroutes.js
+++ bookingroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Booking = require('../models/Booking');
+const Booking = require('./booking');
 
 router.post('/', async (req, res) => {
   try {
--- feedbackroutes.js
+++ feedbackroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Feedback = require('../models/Feedback');
+const Feedback = require('./feedback');
 
 router.post('/', async (req, res) => {
   try {
